b98f07c4da4701e4a8f509d2193f10b6196defe9
Wiktor Phillips <wsphillips@gmail.com>
2021-08-09 00:27:32 +0200

    .

--- gen/generator.jl
+++ gen/generator.jl
@@ -1,67 +1,61 @@
 using Clang.Generators
 using ExprTools, MacroTools
 # using ImPlot.LibCImPlot.CImPlot_jll
-
 using CImGui.CImGui_jll
-
-include_dir = joinpath(CImGui_jll.artifact_dir, "include")
 
 cd(@__DIR__)
 
+const CIMGUI_INCLUDE_DIR = joinpath(CImGui_jll.artifact_dir, "include")
+const CIMPLOT_INCLUDE_DIR = @__DIR__
 const CIMPLOT_H = joinpath(@__DIR__, "cimplot_patched.h") |> normpath
 
 options = load_options(joinpath(@__DIR__, "generator.toml"))
 
-args = ["-I$include_dir", "-DCIMGUI_DEFINE_ENUMS_AND_STRUCTS"]
+args = get_default_args()
+pushfirst!(args, "-DCIMGUI_DEFINE_ENUMS_AND_STRUCTS")
+pushfirst!(args, "-isystem$CIMGUI_INCLUDE_DIR")
+push!(args, "-I$CIMPLOT_INCLUDE_DIR")
 
 # add definitions
-@add_def ImVec2
+@add_def ImVec2 
 @add_def ImVec4
 @add_def ImGuiMouseButton
 @add_def ImGuiKeyModFlags
-@add_def ImS8
+@add_def ImS8 
 @add_def ImU8
-@add_def ImS16
-@add_def ImU16
-@add_def ImS32
+@add_def ImS16 
+@add_def ImU16 
+@add_def ImS32 
 @add_def ImU32
-@add_def ImS64
-@add_def ImU64
-@add_def ImTextureID
-@add_def ImGuiCond
-@add_def ImGuiDragDropFlags
-@add_def ImDrawList
+@add_def ImS64 
+@add_def ImU64 
+@add_def ImTextureID 
+@add_def ImGuiCond 
+@add_def ImGuiDragDropFlags 
+@add_def ImDrawList 
 @add_def ImGuiContext
 
-# jltypes = [Float32, Float64, Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64, UInt64]
-# typedict = Dict(zip(imtypes,jltypes))
-#type_names = ["FloatPtr", "doublePtr", "S8Ptr", "U8Ptr", "S16Ptr", "U16Ptr", "S32Ptr", "U32Ptr", "S64Ptr", "U64Ptr"]  
 imdatatypes = [:Cfloat, :Cdouble, :ImS8, :ImU8, :ImS16, :ImU16, :ImS32, :ImU32, :ImS64, :ImU64]
-plot_types = ["Line", "Scatter", "Stairs", "Shaded", "BarsH", "Bars", "ErrorBarsH", "ErrorBars", "Stems", "VLines", "HLines", "PieChart", "Heatmap", "Histogram", "Histogram2D", "Digital"]
+plot_types = ["Line", "Scatter", "Stairs", "Shaded", "BarsH", "Bars", "ErrorBarsH",
+              "ErrorBars", "Stems", "VLines", "HLines", "PieChart", "Heatmap", "Histogram",
+              "Histogram2D", "Digital"]
 
 ctx = create_context(CIMPLOT_H, args, options)
 build!(ctx, BUILDSTAGE_NO_PRINTING)
 
 #json_string = read("my.json", String)
 #JSON3.read(json_string)
-# @capture(ex, ccall((funsymbol_, libcimplot), rettype_, (argtypes__,), argnames__))
 
 function carg_modify(ex, fun_args)
     if @capture(ex, ccall((funsymbol_, libcimplot), rettype_, (argtypes__,), argnames__))
         for (i, argtype) in enumerate(argtypes)
             if @capture(argtype, Ptr{ptrtype_}) && ptrtype ∈ imdatatypes
                 arg = fun_args[i]
-                @show arg
-                if @capture(fun_args[i], sym_::Ptr{sigptrtype_})
-                    if sigptrtype == ptrtype
-                    fun_args[i] = :($sym::Union{Ptr{$ptrtype},Ref{$ptrtype},AbstractArray{$ptrtype}})
-                    println("Here")
-                    end
-                elseif @capture(argtype, sym_::Cint)
-                    fun_args[i] = :($sym::Integer) 
-                elseif @capture(argtype, sym_::Cdouble | sym_::Cfloat)
-                    fun_args[i] = :($sym::Real)
-                end
+                fun_args[i] = :($arg::Union{Ptr{$ptrtype},Ref{$ptrtype},AbstractArray{$ptrtype}})
+            elseif @capture(argtype, sym_::Cint)
+                fun_args[i] = :($sym::Integer) 
+            elseif @capture(argtype, sym_::Cdouble | sym_::Cfloat)
+                fun_args[i] = :($sym::Real)
             end
         end
         return :(ccall(($funsymbol, libcimplot), $rettype, $(argtypes...,), $(argnames...)))
