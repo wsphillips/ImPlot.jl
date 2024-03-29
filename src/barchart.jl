ed9d191a8ce0ed043ba3aa0ac49e8a6afc919afa
JamesWrigley <james@puiterwijk.org>
2024-03-29 12:39:50 +0100

    Fix PlotBars() and PlotBarsH() wrappers
    Upstream removed PlotBarsH() in favour of passing ImPlotBarsFlags_Horizontal:
    https://github.com/epezent/implot/blob/f156599faefe316f7dd20fe6c783bf87c8bb6fd9/implot.cpp#L41

--- src/barchart.jl
+++ src/barchart.jl
@@ -6,15 +6,15 @@ function PlotBars(label_id, x::AbstractArray{T1}, y::AbstractArray{T2},
 end
 
 function PlotBars(values::AbstractArray{T}; count::Integer=length(values),
-                  label_id::String="", width=0.67, shift=0.0, offset::Integer=0,
+                  label_id::String="", bar_size=0.67, shift=0.0, offset::Integer=0,
                   stride::Integer=1) where {T<:ImPlotData}
-    return PlotBars(label_id, values, count, width, shift, offset, stride * sizeof(T))
+    return PlotBars(label_id, values, count, bar_size, shift, ImPlotBarsFlags_None, offset, stride * sizeof(T))
 end
 
 function PlotBars(x::AbstractArray{T}, y::AbstractArray{T};
-                  count::Integer=min(length(x), length(y)), label_id::String="", width=0.67,
-                  offset::Integer=0, stride::Integer=1) where {T<:ImPlotData}
-    return PlotBars(label_id, x, y, count, width, offset, stride * sizeof(T))
+                  count::Integer=min(length(x), length(y)), label_id::String="", bar_size=0.67,
+                  shift=0.0, offset::Integer=0, stride::Integer=1) where {T<:ImPlotData}
+    return PlotBars(label_id, x, y, count, bar_size, offset, shift, ImPlotBarsFlags_None, stride * sizeof(T))
 end
 
 function PlotBars(x::AbstractArray{T1}, y::AbstractArray{T2};
@@ -24,20 +24,21 @@ end
 
 # Horizontal bars
 function PlotBarsH(label_id, x::AbstractArray{T1}, y::AbstractArray{T2},
-                   args...) where {T1<:Real,T2<:Real}
-    return PlotBarsH(label_id, promote(x, y)..., args...)
+                   count::Integer, bar_size::Real, shift::Real,
+                   offset::Integer, stride::Integer) where {T1<:Real,T2<:Real}
+    return PlotBars(label_id, promote(x, y)..., count, bar_size, shift, ImPlotBarsFlags_Horizontal, offset, stride)
 end
 
 function PlotBarsH(values::AbstractArray{T}; count::Integer=length(values),
-                   label_id::String="", width=0.67, shift=0.0, offset::Integer=0,
+                   label_id::String="", bar_size=0.67, shift=0.0, offset::Integer=0,
                    stride::Integer=1) where {T<:ImPlotData}
-    return PlotBarsH(label_id, values, count, width, shift, offset, stride * sizeof(T))
+    return PlotBars(label_id, values, count, bar_size, shift, ImPlotBarsFlags_Horizontal, offset, stride * sizeof(T))
 end
 
 function PlotBarsH(x::AbstractArray{T}, y::AbstractArray{T};
                    count::Integer=min(length(x), length(y)), label_id::String="",
-                   width=0.67, offset::Integer=0, stride::Integer=1) where {T<:ImPlotData}
-    return PlotBarsH(label_id, x, y, count, width, offset, stride * sizeof(T))
+                   bar_size=0.67, shift=0.0, offset::Integer=0, stride::Integer=1) where {T<:ImPlotData}
+    return PlotBars(label_id, x, y, count, bar_size, shift, ImPlotBarsFlags_Horizontal, offset, stride * sizeof(T))
 end
 
 function PlotBarsH(x::AbstractArray{T1}, y::AbstractArray{T2};
